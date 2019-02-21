0191164b67e3c566c40e27ab2568cf78277fabbb
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2019-02-21 14:02:36 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+   int i,spaces;
+   char a[100];
+   printf("enter the string\n");
+   scanf("%s",a);
+   for(i=0 ; a[i]!='\0' ; i++)
+     {
+       if (a[i]==' ')
+     {
+     spaces++;
+     }
+     }
+   printf("the no. of spaces in the string is %d\n",spaces);
+   printf("the no. of words in the string is %d\n",spaces+1);
+   getch();
+ }
